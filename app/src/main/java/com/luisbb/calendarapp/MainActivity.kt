58d5c6cc567157a857e47e38fc103c838b99f972
Katia Krut <katiakrut488@gmail.com>
2023-12-19 21:17:18 +0200

    main activity added and login / register logic updated

--- app/src/main/java/com/luisbb/calendarapp/MainActivity.kt
+++ app/src/main/java/com/luisbb/calendarapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.luisbb.calendarapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class MainActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var user: FirebaseUser? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth!!.currentUser;
+        if (user == null) {
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            textView!!.text = user!!.email
+        }
+        button!!.setOnClickListener(View.OnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        })
     }
 }
