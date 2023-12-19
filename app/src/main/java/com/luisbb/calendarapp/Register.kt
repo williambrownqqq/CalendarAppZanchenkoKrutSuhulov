2890c2094f72c00a6bf864e73488705fdf8cdede
Katia Krut <katiakrut488@gmail.com>
2023-12-19 18:27:28 +0200

    xml screens styles updated & register activity updated

--- app/src/main/java/com/luisbb/calendarapp/Register.kt
+++ app/src/main/java/com/luisbb/calendarapp/Register.kt
@@ -1,11 +1,45 @@
 package com.luisbb.calendarapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+
 
 class Register : AppCompatActivity() {
+    private var editTextEmail: TextInputEditText? = null
+    private var editTextPassword:TextInputEditText? = null
+    private var buttonReg: Button? = null
+    private var mAuth: FirebaseAuth? = null
+    private var textView: TextView? = null
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth!!.currentUser
+        if (currentUser != null) {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
+        textView = findViewById(R.id.loginNow);
+        textView!!.setOnClickListener(View.OnClickListener {
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        })
     }
 }
