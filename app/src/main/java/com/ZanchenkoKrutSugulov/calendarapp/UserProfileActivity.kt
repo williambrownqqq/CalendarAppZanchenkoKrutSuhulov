56610d4361676aca8cc9752b1344f8dea6a4b6e5
Katia Krut <katiakrut488@gmail.com>
2023-12-24 15:56:05 +0200

    user profile toggles added

--- app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/UserProfileActivity.kt
+++ app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/UserProfileActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -22,6 +23,12 @@ class UserProfileActivity : AppCompatActivity() {
     private lateinit var userEmailView: TextView
     private var currentUser: FirebaseUser? = null
 
+    private lateinit var editUserEmail: EditText
+    private lateinit var editEmailButton: ImageView
+    private lateinit var textPassword: TextView
+    private lateinit var editPassword: EditText
+    private lateinit var editPasswordButton: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +38,12 @@ class UserProfileActivity : AppCompatActivity() {
         backButton = findViewById(R.id.backFromUserProfile)
         userEmailView = findViewById(R.id.userEmail)
         currentUser = auth.currentUser
+        editUserEmail = findViewById(R.id.editUserEmail)
+        editEmailButton = findViewById(R.id.editEmailButton)
+        textPassword = findViewById(R.id.textPassword)
+        editPassword = findViewById(R.id.editPassword)
+        editPasswordButton = findViewById(R.id.editPasswordButton)
+
 
         loadUserProfile()
         buttonLogout.setOnClickListener {
@@ -40,6 +53,15 @@ class UserProfileActivity : AppCompatActivity() {
 
         backButton.setOnClickListener {
             finish()
+        }
+
+
+        editEmailButton.setOnClickListener {
+            toggleEditing(userEmailView, editUserEmail)
+        }
+
+        editPasswordButton.setOnClickListener {
+            toggleEditing(textPassword, editPassword)
         }
     }
 
@@ -57,7 +79,19 @@ class UserProfileActivity : AppCompatActivity() {
             val userProfile = documentSnapshot.toObject(User::class.java)
             userEmailView.text = userProfile?.email
         }.addOnFailureListener { exception ->
-            Log.w("MainActivity", "Error getting user details: ", exception)
+            Log.w("UserProfileActivity", "Error getting user details: ", exception)
+        }
+    }
+
+    private fun toggleEditing(textView: TextView, editText: EditText) {
+        if (editText.visibility == View.VISIBLE) {
+            textView.text = editText.text
+            textView.visibility = View.VISIBLE
+            editText.visibility = View.GONE
+        } else {
+            editText.setText(textView.text)
+            editText.visibility = View.VISIBLE
+            textView.visibility = View.GONE
         }
     }
 }
