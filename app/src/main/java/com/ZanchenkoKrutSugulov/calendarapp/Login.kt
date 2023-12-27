55ffc7243d0a5abcfcdb2269ad44d860710ef76e
Katia Krut <katiakrut488@gmail.com>
2023-12-27 17:13:07 +0200

    login with google

--- app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/Login.kt
+++ app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/Login.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.ZanchenkoKrutSugulov.calendarapp.dataClasses.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 @RequiresApi(Build.VERSION_CODES.O)
 class Login : AppCompatActivity() {
@@ -23,6 +36,12 @@ class Login : AppCompatActivity() {
     private var progressBar: ProgressBar? = null
     private var textView: TextView? = null
 
+    lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var database: DatabaseReference
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,5 +87,96 @@ class Login : AppCompatActivity() {
                     }
                 }
         })
+
+        configureGoogleSignIn()
+
+        findViewById<ImageView>(R.id.loginWithGoogle).setOnClickListener {
+            signInWithGoogle()
+        }
     }
+
+    private fun configureGoogleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient.signOut()
+    }
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "#ERROR onActivityResult", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "firebaseAuthWithGoogle", Toast.LENGTH_SHORT).show()
+                    startMainActivity()
+//                    checkIfUserExists(task.result?.user)
+                } else {
+                    Toast.makeText(applicationContext, "#ERROR firebaseAuthWithGoogle", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun checkIfUserExists(firebaseUser: FirebaseUser?) {
+        if (!::database.isInitialized) {
+            database = FirebaseDatabase.getInstance().getReference("users")
+        }
+        Log.d("Login", "!DATABASE ${database.toString()}")
+        firebaseUser?.let { user ->
+            Log.d("Login", "!DATABASE USER ${user.uid}")
+            database.child(user.uid).get().addOnSuccessListener {
+                Log.d("Login", "!DATABASE USER IT ${it.toString()}")
+                if (!it.exists()) {
+                    Log.d("Login", "User does not exist. Creating new user in database.")
+                    createUserInDatabase(user)
+                } else {
+                    Toast.makeText(applicationContext, "User already exists in database.", Toast.LENGTH_SHORT).show()
+                }
+                startMainActivity()
+            }.addOnFailureListener {
+                Toast.makeText(applicationContext, "Failed to check user existence: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun createUserInDatabase(firebaseUser: FirebaseUser) {
+        val user = User(
+            uid = firebaseUser.uid,
+            email = firebaseUser.email
+            // Добавьте другие поля, если требуется
+        )
+
+        database.child(firebaseUser.uid).setValue(user).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(applicationContext, "User created in database", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Failed to create user in database: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
