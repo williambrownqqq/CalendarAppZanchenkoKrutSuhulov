52ace33f1d46472c2f75ce9b2f18405452670f52
kekekek <katiakrut448@gmail.com>
2023-12-31 16:10:13 +0200

    calendars activity updated & styles updated

--- app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/CalendarActivity.kt
+++ app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/CalendarActivity.kt
@@ -6,11 +6,14 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ZanchenkoKrutSugulov.calendarapp.dataClasses.Calendar
+import com.ZanchenkoKrutSugulov.calendarapp.database.dao.CalendarDao
+import com.google.firebase.auth.FirebaseAuth
+import com.ZanchenkoKrutSugulov.calendarapp.database.dao.CalendarDatabase
 
 class CalendarActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var calendarAdapter: CalendarAdapter
-
+    private lateinit var calendarDao: CalendarDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_actiivity)
@@ -18,19 +21,24 @@ class CalendarActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerViewCalendars)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val calendars = loadCalendars()
-
-        calendarAdapter = CalendarAdapter(calendars, ::onEditCalendar, ::onDeleteCalendar)
-        recyclerView.adapter = calendarAdapter
+        calendarDao = CalendarDatabase
+        loadCalendars()
     }
 
     private fun onEditCalendar(calendar: Calendar) {
     }
 
     private fun onDeleteCalendar(calendar: Calendar) {
+        calendarDao.deleteCalendar(calendar.calendarId)
+        loadCalendars()
     }
 
-    private fun loadCalendars(): List<Calendar> {
-        return listOf()
+    private fun loadCalendars() {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid ?: return
+        calendarDao.getCalendars(userId) { calendars ->
+            calendarAdapter = CalendarAdapter(calendars, ::onEditCalendar, ::onDeleteCalendar)
+            recyclerView.adapter = calendarAdapter
+        }
     }
+
 }
