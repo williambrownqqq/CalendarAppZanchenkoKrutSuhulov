bc0bd9ebabe6232cf324381f7e5a74cd6f7f2b7c
kekekek <katiakrut448@gmail.com>
2024-01-04 13:42:40 +0200

    merge google sync and firebase events

--- app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/MainActivity.kt
+++ app/src/main/java/com/ZanchenkoKrutSugulov/calendarapp/MainActivity.kt
@@ -8,9 +8,11 @@ import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
@@ -30,7 +32,23 @@ import com.ZanchenkoKrutSugulov.calendarapp.utils.getYearsArray
 import com.ZanchenkoKrutSugulov.calendarapp.utils.localDateToEpochSecond
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
+import java.util.UUID
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.api.client.util.DateTime
+import java.time.Instant
+import java.time.ZoneId
+import com.google.api.services.calendar.model.CalendarListEntry
+import androidx.appcompat.app.AlertDialog
+import com.google.api.services.calendar.model.Event
+
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
@@ -40,6 +58,9 @@ class MainActivity : AppCompatActivity() {
     private var currentUser: FirebaseUser? = null
     private var monthEvents: LiveData<List<DateEvent>> = MutableLiveData()
     var currentDate: ZonedDateTime = ZonedDateTime.now()
+    private lateinit var syncButton: Button
+
+    private var calendarService: Calendar? = null
 
     private var calendarId = ""
 
@@ -51,6 +72,8 @@ class MainActivity : AppCompatActivity() {
         buttonShowProfile = findViewById(R.id.buttonShowProfile)
         buttonMenu = findViewById(R.id.buttonOpenCalendars)
         currentUser = auth.currentUser
+        syncButton = findViewById(R.id.buttonSyncWithGoogle)
+
 
         currentUser?.let {
             getPrimaryCalendarForUser(it.uid) { calendarEvent ->
@@ -75,6 +98,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, Login::class.java))
             finish()
         }
+
+        syncButton.setOnClickListener {
+            listCalendars()
+        }
+
+        setupGoogleCalendarApi()
+
         setupSpinners()
     }
 
@@ -121,6 +151,125 @@ class MainActivity : AppCompatActivity() {
         Log.d("getMonthEvents", "!monthEvents: ${monthEvents.value?.map { it }}")
         observeMonthEvents()
     }
+
+
+    private fun setupGoogleCalendarApi() {
+        val credential = GoogleAccountCredential.usingOAuth2(
+            this, listOf(CalendarScopes.CALENDAR_READONLY)
+        ).setBackOff(ExponentialBackOff())
+
+        val googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this)
+        credential.selectedAccount = googleSignInAccount?.account
+
+        calendarService = Calendar.Builder(
+            NetHttpTransport(),
+            JacksonFactory.getDefaultInstance(),
+            credential
+        ).setApplicationName(getString(R.string.app_name))
+            .build()
+        Log.d("!GOOGLE API", "!GOOGLE API SERVICE: ${calendarService}")
+
+    }
+    private fun listCalendars() {
+        val thread = Thread {
+            try {
+                val calendarList = calendarService?.calendarList()?.list()?.execute()
+                val calendars = calendarList?.items
+                Log.d("MainActivity", "!CALENDARS SYNC: ${calendars?.map { it.id }}")
+
+                runOnUiThread {
+                    if (calendars != null) {
+                        showCalendarDialog(calendars)
+                    }
+                }
+            } catch (e: Exception) {
+                runOnUiThread {
+                    Toast.makeText(this, "Error fetching calendar list: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+        thread.start()
+    }
+
+    private fun showCalendarDialog(calendars: List<CalendarListEntry>) {
+        val calendarNames = calendars.map { it.summary }.toTypedArray()
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Choose a Calendar")
+        builder.setItems(calendarNames) { _, which ->
+            loadEventsFromGoogleCalendar(calendars[which].id)
+        }
+        builder.show()
+    }
+
+    private fun fetchEventsFromCalendar(calendarId: String, year: Int, month: Int) {
+        val thread = Thread {
+            try {
+                val startOfMonth = java.time.LocalDate.of(year, month, 1).toString() + "T00:00:00-07:00"
+                val endOfMonth = java.time.LocalDate.of(year, month, 1).withDayOfMonth(
+                    java.time.LocalDate.of(year, month, 1).lengthOfMonth()
+                ).toString() + "T23:59:59-07:00"
+                val events = calendarService?.events()
+                    ?.list(calendarId)
+                    ?.setTimeMin(DateTime(startOfMonth))
+                    ?.setTimeMax(DateTime(endOfMonth))
+                    ?.execute()
+
+                Log.d("MainActivity", "!CALENDARS SYNC: Events: ${events?.items?.map { it}}")
+
+
+                val items = events?.items
+                if (items != null) {
+                    if (items.isNotEmpty()) {
+                        for (event in items) {
+                            val eventStart = event.start.dateTime ?: event.start.date
+                            val localDate: LocalDateTime = getLocalDateTime(eventStart)
+                            Log.d("MainActivity", "!CALENDARS SYNC: Converting: $localDate")
+                            val newEvent = convertEventFromGoogleCalendar(localDate, event)
+                            Log.d("MainActivity", "!CALENDARS SYNC: newEvent: ${newEvent.toString()}")
+
+                            if (newEvent != null) {
+                                EventDatabase.insertDateEvent(newEvent)
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                runOnUiThread {
+                    Toast.makeText(this, "Error fetching events: ${e.message}", Toast.LENGTH_LONG)
+                        .show()
+                    Log.d("MainActivity", "!CALENDARS SYNC: Error fetching events: ${e.message}")
+
+                }
+            }
+        }
+        thread.start()
+    }
+    private fun getLocalDateTime(eventStart: DateTime): LocalDateTime {
+        return Instant.ofEpochMilli(eventStart.value).atZone(ZoneId.systemDefault()).toLocalDateTime()
+    }
+    private fun convertEventFromGoogleCalendar(localDate: LocalDateTime?, event: Event): DateEvent? {
+        Log.d("MainActivity", "!CALENDARS SYNC: convertEventFromGoogleCalendar: ${localDate?.year}")
+        return localDate?.let {
+            DateEvent(
+                year = it.year,
+                month = it.month.value,
+                day = it.dayOfMonth,
+                hour = it.hour,
+                minute = it.minute,
+                name = event.summary,
+                description = event.description,
+                calendarId = calendarId,
+                id = UUID.randomUUID().toString(),
+            )
+        }
+    }
+
+    private fun loadEventsFromGoogleCalendar(calendarId: String) {
+        val selectedYear = currentDate.year
+        val selectedMonth = currentDate.monthValue
+        fetchEventsFromCalendar(calendarId, selectedYear, selectedMonth)
+    }
+
 
     private fun setupSpinners() {
         val monthSpinner = findViewById<Spinner>(R.id.spnMonth)
